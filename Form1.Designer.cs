12d4e8acef26997ef2b502b1630f1563d2ba5b46
Jahrai Haile <jahrai.haile@hgs.hiddengeniusproject.org>
2020-11-20 15:46:07 -0800

    pushing my code to git

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace automobile
+{
+    partial class FrmAutomibilecost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ca = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Label = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.IntxtCarNote = new System.Windows.Forms.TextBox();
+            this.inTxtRegistrationFees = new System.Windows.Forms.TextBox();
+            this.inTxtCarInsurance = new System.Windows.Forms.TextBox();
+            this.inTxtBasicRepairs = new System.Windows.Forms.TextBox();
+            this.inTxtOilChange = new System.Windows.Forms.TextBox();
+            this.InTxtGas = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.outTxtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(48, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "car insurance";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Ca
+            // 
+            this.Ca.AutoSize = true;
+            this.Ca.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Ca.Location = new System.Drawing.Point(73, 9);
+            this.Ca.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Ca.Name = "Ca";
+            this.Ca.Size = new System.Drawing.Size(46, 13);
+            this.Ca.TabIndex = 1;
+            this.Ca.Text = "CarNote";
+            this.Ca.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(47, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Basic Repairs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(30, 189);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Regristration fees";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Label.Location = new System.Drawing.Point(60, 121);
+            this.Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(59, 13);
+            this.Label.TabIndex = 4;
+            this.Label.Text = "Oil Change";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Label2.Location = new System.Drawing.Point(95, 75);
+            this.Label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(24, 13);
+            this.Label2.TabIndex = 5;
+            this.Label2.Text = "gas";
+            // 
+            // IntxtCarNote
+            // 
+            this.IntxtCarNote.HideSelection = false;
+            this.IntxtCarNote.Location = new System.Drawing.Point(131, 11);
+            this.IntxtCarNote.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.IntxtCarNote.Name = "IntxtCarNote";
+            this.IntxtCarNote.Size = new System.Drawing.Size(287, 20);
+            this.IntxtCarNote.TabIndex = 6;
+            // 
+            // inTxtRegistrationFees
+            // 
+            this.inTxtRegistrationFees.Location = new System.Drawing.Point(131, 189);
+            this.inTxtRegistrationFees.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inTxtRegistrationFees.Name = "inTxtRegistrationFees";
+            this.inTxtRegistrationFees.Size = new System.Drawing.Size(287, 20);
+            this.inTxtRegistrationFees.TabIndex = 7;
+            // 
+            // inTxtCarInsurance
+            // 
+            this.inTxtCarInsurance.Location = new System.Drawing.Point(131, 39);
+            this.inTxtCarInsurance.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inTxtCarInsurance.Name = "inTxtCarInsurance";
+            this.inTxtCarInsurance.Size = new System.Drawing.Size(287, 20);
+            this.inTxtCarInsurance.TabIndex = 8;
+            // 
+            // inTxtBasicRepairs
+            // 
+            this.inTxtBasicRepairs.Location = new System.Drawing.Point(131, 151);
+            this.inTxtBasicRepairs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inTxtBasicRepairs.Name = "inTxtBasicRepairs";
+            this.inTxtBasicRepairs.Size = new System.Drawing.Size(287, 20);
+            this.inTxtBasicRepairs.TabIndex = 9;
+            // 
+            // inTxtOilChange
+            // 
+            this.inTxtOilChange.Location = new System.Drawing.Point(131, 118);
+            this.inTxtOilChange.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inTxtOilChange.Name = "inTxtOilChange";
+            this.inTxtOilChange.Size = new System.Drawing.Size(287, 20);
+            this.inTxtOilChange.TabIndex = 10;
+            // 
+            // InTxtGas
+            // 
+            this.InTxtGas.Location = new System.Drawing.Point(131, 72);
+            this.InTxtGas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.InTxtGas.Name = "InTxtGas";
+            this.InTxtGas.Size = new System.Drawing.Size(287, 20);
+            this.InTxtGas.TabIndex = 11;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCalculate.Location = new System.Drawing.Point(295, 235);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(123, 24);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.submitExspenses);
+            // 
+            // outTxtResults
+            // 
+            this.outTxtResults.Location = new System.Drawing.Point(58, 282);
+            this.outTxtResults.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outTxtResults.Multiline = true;
+            this.outTxtResults.Name = "outTxtResults";
+            this.outTxtResults.Size = new System.Drawing.Size(360, 103);
+            this.outTxtResults.TabIndex = 13;
+            this.outTxtResults.Text = "***Final Results***\r\nMonthly Cost $\r\nAnnual Cost $\r\n\r\n";
+            // 
+            // FrmAutomibilecost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 406);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Ca);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outTxtResults);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.InTxtGas);
+            this.Controls.Add(this.inTxtOilChange);
+            this.Controls.Add(this.inTxtBasicRepairs);
+            this.Controls.Add(this.inTxtCarInsurance);
+            this.Controls.Add(this.inTxtRegistrationFees);
+            this.Controls.Add(this.IntxtCarNote);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FrmAutomibilecost";
+            this.Text = "Automobile Cost Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Ca;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.TextBox IntxtCarNote;
+        private System.Windows.Forms.TextBox inTxtRegistrationFees;
+        private System.Windows.Forms.TextBox inTxtCarInsurance;
+        private System.Windows.Forms.TextBox inTxtBasicRepairs;
+        private System.Windows.Forms.TextBox inTxtOilChange;
+        private System.Windows.Forms.TextBox InTxtGas;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox outTxtResults;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace automobile
+{
+    public partial class FrmAutomibilecost : Form
+    {
+        public FrmAutomibilecost()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitExspenses(object sender, EventArgs e)
+        {
+            /*Get User Information From Text Input */
+            String CarNoteStr = IntxtCarNote.Text;
+            String CarInsuranceStr = inTxtCarInsurance.Text;
+            String gasStr= InTxtGas.Text;
+            String OilChangeStr= inTxtOilChange.Text;
+            String BasicRepairStr= inTxtBasicRepairs.Text;
+            String RegristrationFeesStr= inTxtRegistrationFees.Text;
+
+            Double CarNoteDbl = Convert.ToDouble(CarNoteStr);
+            Double CarInsuranceDbl = Convert.ToDouble(CarInsuranceStr);
+            Double gasDbl = Convert.ToDouble(gasStr);
+            Double OilChangeDbl = Convert.ToDouble(OilChangeStr);
+            Double BasicRepairDbl = Convert.ToDouble(BasicRepairStr);
+            Double RegristrationFeesDbl = Convert.ToDouble(RegristrationFeesStr);
+
+            Double monthlyExspenses = CarNoteDbl + CarInsuranceDbl + gasDbl + OilChangeDbl + RegristrationFeesDbl;
+            Double yearlyExpesse = monthlyExspenses * 12;
+
+            outTxtResults.Text = "";
+            
+                
+            outTxtResults.Text = "***Final Results***" + Environment.NewLine;
+            outTxtResults.AppendText("Total Monthly Cost: " + monthlyExspenses.ToString ("c") + Environment.NewLine);
+            outTxtResults.AppendText("Total yearly Cost: " + yearlyExpesse.ToString("c") + Environment.NewLine);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
